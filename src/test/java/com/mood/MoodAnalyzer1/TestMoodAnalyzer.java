7fcf4e3b98e77da2d29e2981c74345cf639e050e
Subhashree naik <subhashree.naik@gmail.com>
2022-03-14 23:21:19 +0530

    Inform user invalid mood using custom exception

--- src/test/java/com/mood/MoodAnalyzer1/TestMoodAnalyzer.java
+++ src/test/java/com/mood/MoodAnalyzer1/TestMoodAnalyzer.java
@@ -9,28 +9,38 @@ public class TestMoodAnalyzer {
 	 * 
 	 * This test case to check whether the user is happy.
 	 * If happy then it will be true ,if sad then it will be false
+	 * @throws MoodAnalyzerException 
 	 */
 	
 	@Test
-	public void testMoodAnalyzerSad() {
+	public void testMoodAnalyzerSad() throws MoodAnalyzerException {
+		
 		MoodAnalyzer mood=new MoodAnalyzer("I am in sad mood");
 		assertEquals("sad",mood.enalyzeEmotion() );
-	    
-	}
+	  
+}
 	@Test
-	public void testMoodAnalyzerHappy() {
+	public void testMoodAnalyzerHappy() throws MoodAnalyzerException {
+		
 		MoodAnalyzer mood=new MoodAnalyzer("I am in any mood");
 		assertEquals("happy",mood.enalyzeEmotion() );
 	    
+	
+		
 	}
 	/*
-	 * UC2 checking null pointer exception here
+	 * UC3 Inform user if enter invalid mood
 	 */
 	@Test
-	public void testMoodAnalyzerNull() {
+	public void testMoodAnalyzerNull() throws MoodAnalyzerException {
+		try {
 		MoodAnalyzer mood=new MoodAnalyzer(null);
 		assertEquals("Invalid mood",mood.enalyzeEmotion() );
 	    
+	}
+		catch(MoodAnalyzerException e) {
+			System.out.println("empty mood");
+		}
 	}
 
 }
